ed8bb00e3f84a0fd3c98c32620be78f4f85ea584
Praveenhalder <164347311+Praveenhalder@users.noreply.github.com>
2026-03-08 19:38:00 +0530

    Upload source code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shri Premanand Ji
+Shri Premanand Ji - Open Source Android App
---
+++ android/app/src/main/java/com/halder/shripremanandji/MainActivity.java
@@ -0,0 +1,5 @@
+package com.halder.shripremanandji;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
